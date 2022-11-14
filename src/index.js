40d8653662f489ae7c99562da09a5f8e03335fde
Yerin <zlekfl0311@gmail.com>
2022-11-14 18:01:11 +0900

    JS Redux ToDoList

--- src/index.js
+++ src/index.js
@@ -1,72 +1,74 @@
 import { createStore } from 'redux';
-// ------------- vanilla JS_Counter -------------
-// const add = document.getElementById("add");
-// const minus = document.getElementById("minus");
-// const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-// let count = 0;
-// number.innerText = count;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-// const updateText = () => {
-//   number.innerText = count;
-// };
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
 
-// const handleAdd = () => {
-//   count = count + 1;
-//   updateText();
-// };
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
 
-// const handleMinus = () => {
-//   count = count - 1;
-//   updateText();
-// };
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
+    case DELETE_TODO:
+      const cleaned = state.filter(toDo => toDo.id !== action.id);
+      return cleaned;
+    default:
+      return state;
+  }
+};
 
-// add.addEventListener("click", handleAdd);
-// minus.addEventListener("click", handleMinus);
+const store = createStore(reducer);
 
-// ------------- Pure Redux_Counter -------------
+store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
+};
 
-// const add = document.getElementById("add");
-// const minus = document.getElementById("minus");
-// const number = document.querySelector("span");
+const dispatchDeleteToDo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
 
-// number.innerText = 0;
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
 
-// const ADD = "ADD";
-// const MINUS = "MINUS";
+store.subscribe(paintToDos);
 
-// const countModifier = (count = 0, action) => {
-//   switch (action.type) {
-//     case ADD:
-//       return count + 1;
-//     case MINUS:
-//       return count - 1;
-//     default:
-//       return count;
-//   }
-// };
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
 
-// const countStore = createStore(countModifier);
+form.addEventListener("submit", onSubmit);
 
-// const onChange = () => {
-//   number.innerText = countStore.getState();
-// };
-
-// countStore.subscribe(onChange); // store 변화 감지
-
-
-// //  버튼을 클릭하면 익명함수로 액션 전달
-// add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
-// minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
-
-// ------------- Vanilla JS_ToDoList -------------
-
-
-// ------------- Pure Redux_ToDoList -------------
-
-
-// ------------- React Redux -------------
-
-
-// ------------- Redux Toolkit(RTK) -------------
