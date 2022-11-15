5a4c1335831de67e8e0ddcd1a7eedf6d3924a1e4
Yerin <zlekfl0311@gmail.com>
2022-11-15 11:07:01 +0900

    react redux ToDoList

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; //HashRouter
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
+
 
 function App() {
-    return "App";
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/:id" element={<Detail />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
